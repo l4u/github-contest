7053d72724bc038b164cce336149cdc4d485759c
jbrownlee <jason.brownlee05@gmail.com>
2009-08-20 15:33:12 +1000

    updated knn strategy for testing

--- ruby/knn_strategy.rb
+++ ruby/knn_strategy.rb
@@ -13,7 +13,7 @@ PREDICTION_MAX_REPOS = 10
 STRATEGY_NAME = "kNN"
 K = 10
 SCORE_CUT_OFF = 0
-
+TESTING = true
 
 # larger == better
 def calculate_user_scoring(user, other)
@@ -30,11 +30,11 @@ end
 def calculate_neighbours(user, all_users)
   # score all other users against user of interest
   all_neighbours = Hash.new
-  all_users.values.each do |other| 
+  all_users.each do |other| 
     next if other.id == user.id
     score = calculate_user_scoring(user, other)
     next if score <= SCORE_CUT_OFF
-    all_neighbours[other.id] = score
+    all_neighbours[other.id.to_s] = score
   end
   # order by distance decending 
   nested = all_neighbours.sort {|a,b| b[1]<=>a[1]}
@@ -49,20 +49,20 @@ def calculate_neighbours(user, all_users)
   return neighbours
 end
 
-# returns a list of unique repos ordered by neighbourhood occurance decending 
+# returns a list of unique repo id ordered by neighbourhood occurance decending 
 def rank_missing_repos(user, neighbours, model)
   return nil if neighbours.nil? or neighbours.empty?
   # build a histogram of repo occurance
   occurance_histogram = Hash.new
   neighbours.each do |neighbour_id|
-    neighbour =  model.user_map[neighbour_id]
+    neighbour = model.get_user(neighbour_id)
     neighbour.repositories.each do |repo_id|
       # ensure user does not have it already
       next if user.has_repo?(repo_id)
       # create as needed
-      occurance_histogram[repo_id] = 0 if !occurance_histogram.has_key?(repo_id)
+      occurance_histogram[repo_id.to_s] = 0 if !occurance_histogram.has_key?(repo_id.to_s)
       # increment 
-      occurance_histogram[repo_id] = occurance_histogram[repo_id] + 1
+      occurance_histogram[repo_id.to_s] = occurance_histogram[repo_id.to_s] + 1
     end
   end
   # order by occurance decending
@@ -75,20 +75,20 @@ end
 
 def apply_strategy(model) 
   # process all of the test users
-  model.test_users.each_with_index do |user_id, index|
-    user =  model.user_map[user_id]
+  model.all_test_users.each_with_index do |user_id, index|
+    user = model.get_user(user_id)
     # calculate user neighbours
-    neighbours = calculate_neighbours(user, model.user_map)
+    neighbours = calculate_neighbours(user, model.all_users)
     # rank repo's missing from user
     repos = rank_missing_repos(user, neighbours, model)
     # suggest top <=10 best whereever possible
     next if repos.nil? or repos.empty?
-    repos.each_with_index do |r, i|
+    repos.each_with_index do |repo_id, i|
       break if i >= PREDICTION_MAX_REPOS
-      user.add_prediction(r)
+      user.add_prediction(repo_id)
     end
-    puts " > user (#{index + 1}/#{model.test_users.size}) id=#{user.id} with #{neighbours.size} neighbours was recommended #{user.predicted.size} repos" 
-    break if index > 10
+    puts " > user (#{index + 1}/#{model.all_test_users.size}) id=#{user.id} with #{neighbours.size} neighbours was recommended #{user.predicted.size} repos" 
+    break if (TESTING and index > 10)
   end
   puts "done."
 end
