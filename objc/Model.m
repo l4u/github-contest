e8796a7d6173800a686f9c421baefc9afd43fc62
jasonb <jason.brownlee05@gmail.com>
2009-08-21 20:41:58 +1000

    added repo and model to objc

---
+++ objc/Model.m
@@ -0,0 +1,20 @@
+#import "Model.h"
+
+@implementation Model
+
+-(id) init {
+	self = [super init];	
+	
+	if(self) {
+		
+	}
+	
+	return self;
+}
+
+-(void) dealloc {
+	
+	[super dealloc]; // always last
+}
+
+@end
