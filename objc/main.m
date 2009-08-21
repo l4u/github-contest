97d29e12718eb61a1ee13df23817c5a18b70eb9c
jbrownlee <jason.brownlee05@gmail.com>
2009-08-21 16:56:03 +1000

    added repo to objc

--- objc/main.m
+++ objc/main.m
@@ -7,13 +7,21 @@ Compilation: http://www.cs.indiana.edu/classes/c304/ObjCompile.html
 #import <Foundation/Foundation.h>
 
 #import "User.h"
+#import "Repository.h"
 
 int main(int argc, const char *argv[]) {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
     NSLog(@"Hello world\n");
+
+	// user test
 	User *user = [[User alloc] initWithId:10];
-	// TODO print user id
+	NSLog(@"User id=%i\n", [user userId]);
+
+	// repo test
+	Repository *repo = [[Repository alloc] init];
+	[repo parse:@"123338:DylanFM/roro-faces,2009-05-31,13635"];
+	NSLog(@"Repo id=%i, name=%s, date=%s, parent=%i\n", [repo repoId], [repo fullname], [repo date], [repo parentId]);
 
     [pool drain];
     return 0;
